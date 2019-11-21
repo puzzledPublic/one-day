0eb054df2796f3d30e5ba204d43b6e86023f783d
puzzledPublic <puzzledPublic@gmail.com>
2019-11-21 23:49:37 +0900

    xss filter

--- one-day/src/main/java/com/oneday/sofa/global/config/XssFilter.java
+++ one-day/src/main/java/com/oneday/sofa/global/config/XssFilter.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Component;
 
+//TODO:: JSON 입력에 대해서도 XSS 방어 필요
 //JSON 내용에 대해서는 xssfilter 적용이 안됨. 
 //그래서 JSON 변환을 해주는 HttpMessageConverter 중에 하나인 MappingJackson2HttpMessageConverter을 커스터마이징하여 JSON에 대해 XSS 방어 적용 필요
 @Component
